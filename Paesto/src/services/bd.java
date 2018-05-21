667b0f99201ce2bdfff303ac1cb425caee07dc21
HeIIhound <orlandopereira1233@hotmail.com>
2018-05-21 11:09:32 +0200

    db

---
+++ Paesto/src/services/bd.java
@@ -0,0 +1,43 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class bd {
+
+	public static void main(String[] args){
+		
+		
+			try {
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/almacen","root","1111");
+				
+				PreparedStatement consulta = con.prepareStatement("SELECT * FROM producto");
+				ResultSet resultado = consulta.executeQuery();
+				
+				while(resultado.next()){
+				System.out.println(resultado.getInt(1)+" "+
+								   resultado.getString(2)+" "+
+								   resultado.getString(3)+" "+
+								   resultado.getFloat(4)+" "+
+								   resultado.getString(5));
+				}
+				
+				con.close();
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println(e.getMessage()+"1");
+				
+				
+			} catch (SQLException n){
+				System.out.println(n.getMessage()+"2");
+				
+			}
+	}
+}
