27b5ab6232629e3ce47c1457289504eb271d9407
CarlosVerd <34572410+CarlosVerd@users.noreply.github.com>
2018-05-23 09:53:41 +0200

    conexion servlet con categorias

--- Paesto/src/control/ServletCategoria.java
+++ Paesto/src/control/ServletCategoria.java
@@ -1,11 +1,18 @@
 package control;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Producto;
+import services.Iservices;
+import services.Service;
 
 /**
  * Servlet implementation class ServletCategoria
@@ -14,12 +21,9 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletCategoria extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
+	private Iservices ser = new Service();
+	
 	public ServletCategoria() {
-		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,7 +41,19 @@ public class ServletCategoria extends HttpServlet {
 	}
 
 	protected void continuar(HttpServletRequest request, HttpServletResponse response) {
-
+		ArrayList<Producto> categoria = null;
+		
+		String pasarCategoria=request.getParameter("cat");
+		categoria = ser.listaProductos(pasarCategoria);
+		System.out.println(categoria);
+		request.setAttribute("categoria", categoria);
+		RequestDispatcher view = request.getRequestDispatcher("categorias.jsp");
+		try {
+			view.forward(request, response);
+		} catch (ServletException | IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		
 	}
 
